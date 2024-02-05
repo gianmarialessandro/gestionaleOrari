7bfe4358d36c02b29a606caf3e4d564df1e7faa9
gianmarialessandro <moretti@gianmarialessandro.com>
2024-02-05 08:20:41 +0100

    implementazione logica pulsanti per conferma dati inseriti lato front end

--- frontEnd/src/components/Auth/Signup.jsx
+++ frontEnd/src/components/Auth/Signup.jsx
@@ -12,11 +12,10 @@ const Signup = () => {
   const [emailStd, setEmailStd] = useState(false);
   const [allowPassword, setAllowPassword] = useState(false);
   const [passwordStd, setPasswordStd] = useState(false);
+  const [aziendaStd, setAziendaStd] =useState(false);
 
   const handleNameChange = (e) => {
     setName(e.target.value);
-    setNameStd(!nameStd);
-    setEmailStd(!emailStd);
   };
 
   const handleEmailChange = (e) => {
@@ -25,8 +24,6 @@ const Signup = () => {
 
   const handleConfirmationEmailChange = (e) => {
     setConfirmationEmail(e.target.value);
-    setEmailStd(!emailStd);
-    setPasswordStd(!passwordStd);
     if (e.target.value === email && e.target.value !== "") setAllowEmail(true);
     else setAllowEmail(false);
   };
@@ -39,6 +36,26 @@ const Signup = () => {
     setConfirmationPassword(e.target.value);
     if (e.target.value === password) setAllowPassword(true);
     else setAllowPassword(false);
+  };
+  
+  const handleAziendaChange = (e) => {
+    setAziendaStd(e.target.value);
+  };
+
+  const handleNameClick = (e) => {
+    e.preventDefault();
+    setTimeout(() => {
+      setNameStd(!nameStd);
+      setEmailStd(!emailStd);
+    }, 300);
+  };
+
+  const handleEmailClick = (e) => {
+    e.preventDefault();
+    setTimeout(() => {
+      setEmailStd(!emailStd);
+      setPasswordStd(!passwordStd);
+    }, 300)
   };
 
   const handleFormSubmit = (e) => {
@@ -75,7 +92,7 @@ const Signup = () => {
                       {/* EMAIL */}
                       {emailStd ? (
                         <>
-                          <Form.Group controlId="formEmail">
+                          <Form.Group controlId="formEmail" className="p-3 m-2">
                             <Form.Label>{name}</Form.Label>
                             <Form.Control
                               type="email"
@@ -85,7 +102,7 @@ const Signup = () => {
                             />
                           </Form.Group>
 
-                          <Form.Group controlId="formConfermationEmail">
+                          <Form.Group controlId="formConfermationEmail" className="p-3 m-2">
                             <Form.Control
                               type="email"
                               placeholder="Conferma la tua email"
@@ -109,48 +126,73 @@ const Signup = () => {
                     </>
 
                     {/* PASSWORD */}
-                    <Form.Group controlId="formPassword">
-                      {/* <Form.Label>Password</Form.Label> */}
-                      <Form.Control
-                        type="password"
-                        placeholder="Inserisci la tua password"
-                        value={password}
-                        onChange={handlePasswordChange}
-                      />
-                    </Form.Group>
+                    {passwordStd ? (
+                      <>
+                        <Form.Group controlId="formPassword">
+                          {/* <Form.Label>Password</Form.Label> */}
+                          <Form.Control
+                            type="password"
+                            placeholder="Inserisci la tua password"
+                            value={password}
+                            onChange={handlePasswordChange}
+                          />
+                        </Form.Group>
 
-                    <Form.Group controlId="formConfirmationPassword">
-                      {/* <Form.Label>Conferma Password</Form.Label> */}
-                      <Form.Control
-                        className={
-                          allowPassword ? "text-success" : "text-danger"
-                        }
-                        type="password"
-                        placeholder="Conferma la tua password"
-                        value={confirmationPassword}
-                        onChange={handleConfirmationPasswordChange}
-                      />
-                      {allowPassword ? (
-                        <Form.Text className="text-success">
-                          Passwords corrispondenti
-                        </Form.Text>
-                      ) : (
-                        <Form.Text className="text-danger">
-                          passwords non corrispondenti
-                        </Form.Text>
-                      )}
-                    </Form.Group>
+                        <Form.Group controlId="formConfirmationPassword">
+                          {/* <Form.Label>Conferma Password</Form.Label> */}
+                          <Form.Control
+                            className={
+                              allowPassword ? "text-success" : "text-danger"
+                            }
+                            type="password"
+                            placeholder="Conferma la tua password"
+                            value={confirmationPassword}
+                            onChange={handleConfirmationPasswordChange}
+                          />
+                          {allowPassword ? (
+                            <Form.Text className="text-success">
+                              Passwords corrispondenti
+                            </Form.Text>
+                          ) : (
+                            <Form.Text className="text-danger">
+                              passwords non corrispondenti
+                            </Form.Text>
+                          )}
+                        </Form.Group>
+                      </>
+                    ) : (
+                      <></>
+                    )}
+
+                     {/* AZIENDA */}
+                     {aziendaStd ? (
+                      <Form.Group controlId="formName">
+                        {/* <Form.Label>Nome e Cognome</Form.Label> */}
+                        <Form.Control
+                          type="name"
+                          placeholder="Inserisci il nome della azienda che rappresenti"
+                          value={name}
+                          onChange={handleAziendaChange}
+                        />
+                      </Form.Group>
+                    ) : (
+                      <div></div>
+                    )}
 
                     {/* PROCEDI INVIO */}
                     {nameStd ? (
-                      <Button variant="info" type="button">
+                      <Button
+                        variant="info"
+                        type="button"
+                        onClick={handleNameClick}
+                      >
                         prossimo
                       </Button>
                     ) : (
                       <></>
                     )}
                     {emailStd ? (
-                      <Button variant="info" type="button">
+                      <Button variant="info" type="button" onClick={handleEmailClick}>
                         prossimo
                       </Button>
                     ) : (
